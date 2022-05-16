d877652e344e8652ce0aaede97cfc5e4fd9c44e6
Med Bouya <mdbouya88@gmail.com>
2022-05-16 23:31:13 +0000

    Allowing MIME types

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import mimetypes
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,6 @@ REST_FRAMEWORK = {
     # Adding filter options by default using django_filters app
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
+
+# MIME Types for production
+mimetypes.add_type("text/css", ".css", True)
